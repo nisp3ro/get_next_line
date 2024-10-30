1adbacebca93a8d1c9411f688456885b3d8a2127
Martin Rubal Coelho <mrubal-c@c3r13s1.42madrid.com>
2024-10-30 20:34:45 +0100

    gnl

---
+++ get_next_line_bonus.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrubal-c <mrubal-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/28 13:58:37 by mrubal-c          #+#    #+#             */
+/*   Updated: 2024/09/30 12:01:04 by mrubal-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+//gnl
+char	*get_next_line(int fd);
+char	*ft_read_till_n(int fd, char *save);
+char	*ft_get_line(char *save);
+char	*ft_subs_line_from_save(char *save);
+char	*ft_free(char **str);
+
+//moddedlbft
+char	*ft_strjoin_modded(char *s1, char *s2);
+
+//libft
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int tofind);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
